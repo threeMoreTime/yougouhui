9da91e40eefa075b233f5fd56c6e044294e7fc47
youqun <1224523918@qq.com>
2023-05-27 16:21:16 +0800

    用getter简化List中的goodsList，attrsList,trademarkList数据

--- src/store/search/index.js
+++ src/store/search/index.js
@@ -20,7 +20,19 @@ const actions = {
   },
 };
 // 计算属性，在项目当中是为了简化数据而生的
-const getters = {};
+const getters = {
+  // state形参是当前仓库的state，不是大仓库的state
+  goodsList(state){
+    return state.searchlist.goodsList;
+  },
+  attrsList(state){
+    return state.searchlist.attrsList;
+  },
+  trademarkList(state){
+    return state.searchlist.trademarkList;
+  }
+};
+
 
 export default {
   state,
