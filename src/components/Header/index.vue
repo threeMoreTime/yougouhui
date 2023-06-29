10193c282005ced3262cc372b6f6552da4be1955
zhangsiqiang <1368376439@qq.com>
2023-06-29 17:44:43 +0800

    用户信息渲染header组件(未完成)

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,179 +1,187 @@
 <template>
-	<header class="header">
-		<!-- 头部的第一行 -->
-		<div class="top">
-			<div class="container">
-				<div class="loginList">
-					<p>尚品汇欢迎您！</p>
-					<p>
-						<span>请</span>
-						<!-- <a href="###">登录</a> -->
-						<!-- <a href="###" class="register">免费注册</a> -->
-						<router-link to="/login">登录</router-link>
-						<router-link class="register" to="/register">免费注册</router-link>
-					</p>
-				</div>
-				<div class="typeList">
-					<a href="###">我的订单</a>
-					<a href="###">我的购物车</a>
-					<a href="###">我的尚品汇</a>
-					<a href="###">尚品汇会员</a>
-					<a href="###">企业采购</a>
-					<a href="###">关注尚品汇</a>
-					<a href="###">合作招商</a>
-					<a href="###">商家后台</a>
-				</div>
-			</div>
-		</div>
-		<!--头部第二行 搜索区域-->
-		<div class="bottom">
-			<h1 class="logoArea">
-				<router-link class="logo" to="/home">
-					<img src="./images/logo.png" alt="">
-				</router-link>
-			</h1>
-			<div class="searchArea">
-				<form action="###" class="searchForm">
-					<input type="text" id="autocomplete" class="input-error input-xxlarge" v-model="keyword" />
-					<button @click="goSearch" class="sui-btn btn-xlarge btn-danger" type="button">搜索</button>
-				</form>
-			</div>
-		</div>
-	</header>
+  <header class="header">
+    <!-- 头部的第一行 -->
+    <div class="top">
+      <div class="container">
+        <!-- 没有token的头部信息 -->
+        <div class="loginList" v-if="!getNickName">
+          <p>尚品汇欢迎您！</p>
+          <p>
+            <span>请</span>
+            <router-link to="/login">登录</router-link>
+            <router-link class="register" to="/register">免费注册</router-link>
+          </p>
+        </div>
+        <!-- 有token的头部信息 -->
+        <div class="loginList" v-else>
+          <p>尚品汇欢迎您！</p>
+          <p>
+            <router-link to="/home">{{ getNickName }}</router-link>
+            <router-link class="register" to="/home">退出登陆</router-link>
+          </p>
+        </div>
+
+        <div class="typeList">
+          <a href="###">我的订单</a>
+          <a href="###">我的购物车</a>
+          <a href="###">我的尚品汇</a>
+          <a href="###">尚品汇会员</a>
+          <a href="###">企业采购</a>
+          <a href="###">关注尚品汇</a>
+          <a href="###">合作招商</a>
+          <a href="###">商家后台</a>
+        </div>
+      </div>
+    </div>
+    <!--头部第二行 搜索区域-->
+    <div class="bottom">
+      <h1 class="logoArea">
+        <router-link class="logo" to="/home">
+          <img src="./images/logo.png" alt="" />
+        </router-link>
+      </h1>
+      <div class="searchArea">
+        <form action="###" class="searchForm">
+          <input
+            type="text"
+            id="autocomplete"
+            class="input-error input-xxlarge"
+            v-model="keyword"
+          />
+          <button @click="goSearch" class="sui-btn btn-xlarge btn-danger" type="button">
+            搜索
+          </button>
+        </form>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-	name: '',
-	data() {
-		return {
-			keyword: '',
-		}
-	},
-	methods: {
-		// 路由传递参数 query和params
-		goSearch() {
-			const params = { keyword: this.keyword || undefined };
-			const query = this.$route.query;
-			this.$router.push({ name: 'search', params, query });
-			console.log(this.keyword);
-
-		},
-
-		// goSearch() {
-		//   const currentRoute = this.$router.currentRoute;
-		//   if (currentRoute.name === "search" && currentRoute.params.keyword === this.keyword&&currentRoute.query.K === this.keyword.toUpperCase()) {
-		//     return; // 避免重复导航
-		//   }
-		//   this.$router.push({ name: "search", params: { keyword: this.keyword }, query: { K: this.keyword.toUpperCase() } });
-		// }
-
-	},
-	mounted() {
-		// 通过全局事件通信清楚搜索栏中的params参数
-		this.$bus.$on('clear', () => {
-			console.log('11111');
-			this.keyword = '';
-		})
-	},
-
-}
+  name: "",
+  data() {
+    return {
+      keyword: "",
+    };
+  },
+  methods: {
+    // 路由传递参数 query和params
+    goSearch() {
+      const params = { keyword: this.keyword || undefined };
+      const query = this.$route.query;
+      this.$router.push({ name: "search", params, query });
+      console.log(this.keyword);
+    },
+  },
+  mounted() {
+    this.$store.dispatch("getUserInfo");
+    // 通过全局事件通信清楚搜索栏中的params参数
+    this.$bus.$on("clear", () => {
+      console.log("11111");
+      this.keyword = "";
+    });
+  },
+  computed: {
+    getNickName() {
+      return this.$store.state.user.userinfo.nickName;
+    },
+  },
+};
 </script>
 
 <style scoped lang="less">
 .header {
-	&>.top {
-		background-color: #eaeaea;
-		height: 30px;
-		line-height: 30px;
+  & > .top {
+    background-color: #eaeaea;
+    height: 30px;
+    line-height: 30px;
 
-		.container {
-			width: 1200px;
-			margin: 0 auto;
-			overflow: hidden;
+    .container {
+      width: 1200px;
+      margin: 0 auto;
+      overflow: hidden;
 
-			.loginList {
-				float: left;
+      .loginList {
+        float: left;
 
-				p {
-					float: left;
-					margin-right: 10px;
+        p {
+          float: left;
+          margin-right: 10px;
 
-					.register {
-						border-left: 1px solid #b3aeae;
-						padding: 0 5px;
-						margin-left: 5px;
-					}
-				}
-			}
+          .register {
+            border-left: 1px solid #b3aeae;
+            padding: 0 5px;
+            margin-left: 5px;
+          }
+        }
+      }
 
-			.typeList {
-				float: right;
+      .typeList {
+        float: right;
 
-				a {
-					padding: 0 10px;
+        a {
+          padding: 0 10px;
 
-					&+a {
-						border-left: 1px solid #b3aeae;
-					}
-				}
+          & + a {
+            border-left: 1px solid #b3aeae;
+          }
+        }
+      }
+    }
+  }
 
-			}
+  & > .bottom {
+    width: 1200px;
+    margin: 0 auto;
+    overflow: hidden;
 
-		}
-	}
+    .logoArea {
+      float: left;
 
-	&>.bottom {
-		width: 1200px;
-		margin: 0 auto;
-		overflow: hidden;
+      .logo {
+        img {
+          width: 175px;
+          margin: 25px 45px;
+        }
+      }
+    }
 
-		.logoArea {
-			float: left;
+    .searchArea {
+      float: right;
+      margin-top: 35px;
 
-			.logo {
-				img {
-					width: 175px;
-					margin: 25px 45px;
-				}
-			}
-		}
+      .searchForm {
+        overflow: hidden;
 
-		.searchArea {
-			float: right;
-			margin-top: 35px;
+        input {
+          box-sizing: border-box;
+          width: 490px;
+          height: 32px;
+          padding: 0px 4px;
+          border: 2px solid #ea4a36;
+          float: left;
 
-			.searchForm {
-				overflow: hidden;
+          &:focus {
+            outline: none;
+          }
+        }
 
-				input {
-					box-sizing: border-box;
-					width: 490px;
-					height: 32px;
-					padding: 0px 4px;
-					border: 2px solid #ea4a36;
-					float: left;
+        button {
+          height: 32px;
+          width: 68px;
+          background-color: #ea4a36;
+          border: none;
+          color: #fff;
+          float: left;
+          cursor: pointer;
 
-					&:focus {
-						outline: none;
-					}
-				}
-
-				button {
-					height: 32px;
-					width: 68px;
-					background-color: #ea4a36;
-					border: none;
-					color: #fff;
-					float: left;
-					cursor: pointer;
-
-					&:focus {
-						outline: none;
-					}
-				}
-			}
-		}
-	}
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
+  }
 }
 </style>
