bea8ae47839ce74fe00edd5671a927e3a2abe6ca
zhangsiqiang <1368376439@qq.com>
2023-07-05 15:49:25 +0800

    引入和配置elementUI库

--- src/main.js
+++ src/main.js
@@ -10,19 +10,34 @@ import Carousel from "@/components/Carousel";
 import Pagination from "@/components/Pagination"
 // 引入仓库
 import store from "@/store";
+// 引入elementUI
+import { Button,  MessageBox ,Select} from 'element-ui';
 // 第一个参数：全局组件的名字 第二个参数 是哪个组件
 Vue.component(TypeNav.name, TypeNav);
 Vue.component(Carousel.name, Carousel);
 Vue.component(Pagination.name,Pagination)
+// 使用插件
+Vue.use(Button)
+Vue.use(Select)
+Vue.use(router)
+// 另一种引用 直接挂在原型上面
+Vue.prototype.$msgbox = MessageBox;
+Vue.prototype.$alert = MessageBox.alert;
+
 Vue.use(router);
 // 引入mockServe.js---mock数据
 import "@/mock/mockServe";
+// 配置接口api文件
+import * as API from '@/api'
 Vue.config.productionTip = false;
+
 // 注册路由信息 使组件身上都具有$router和$route属性
 new Vue({
   render: (h) => h(App),
   beforeCreate() {
     Vue.prototype.$bus = this;
+    // 让所有组件都能自由访问接口
+    Vue.prototype.$API = API;
   },
   router,
   store,
